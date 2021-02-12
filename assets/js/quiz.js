e6c806cbd6f710ab556fc972a3c9457ff80dd6b7
gammaled <guleduni@yahoo.co.uk>
2021-02-12 19:08:20 +0000

    defined variables 'score' and 'questionCounter' with value of 0

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,3 +3,6 @@ const answers = Array.from(document.querySelectorAll('.answer-container'));
 const progressText = document.querySelector('#progress-text');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progress-bar-full');
+
+let questionCounter = 0;
+let score = 0;
