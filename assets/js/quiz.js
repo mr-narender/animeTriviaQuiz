a23accc7f22d0a3ecbeafe456b0d77f90420027d
gammaled <guleduni@yahoo.co.uk>
2021-02-12 18:45:15 +0000

    defined variable 'progressBarFull' to target document object with id #progress-bar-full

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,3 +2,4 @@ const question = document.querySelector('#question');
 const answers = Array.from(document.querySelectorAll('.answer-container'));
 const progressText = document.querySelector('#progress-text');
 const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progress-bar-full');
