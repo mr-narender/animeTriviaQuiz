572aa4e5d9a6ea1df9255f58f8a2ac1901acfc6e
gammaled <guleduni@yahoo.co.uk>
2021-02-12 18:40:05 +0000

    defined variable 'progressText' to target document object with id #progress-text

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,2 +1,3 @@
 const question = document.querySelector('#question');
 const answers = Array.from(document.querySelectorAll('.answer-container'));
+const progressText = document.querySelector('#progress-text');
