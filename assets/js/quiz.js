fdff6aa04bb016986ce698484337cf15e5031423
gammaled <guleduni@yahoo.co.uk>
2021-02-12 18:43:31 +0000

    defined variable scoreText to target document object with id '#score'

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,3 +1,4 @@
 const question = document.querySelector('#question');
 const answers = Array.from(document.querySelectorAll('.answer-container'));
 const progressText = document.querySelector('#progress-text');
+const scoreText = document.querySelector('#score');
