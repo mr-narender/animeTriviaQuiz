f57107ba6676a53b178e5d31df98aa749d98ddfd
gammaled <guleduni@yahoo.co.uk>
2021-02-13 18:10:46 +0000

    commented out code between line 16 and 22 which are in function gQuestion()

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,5 +1,5 @@
 const question = document.querySelector('#question');
-const answers = Array.from(document.querySelectorAll('.answer-text'));
+const answers = document.querySelectorAll('.answer-text');
 const progressText = document.querySelector('#progress-text');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progress-bar-full');
@@ -10,14 +10,15 @@ let currentQuestion = {};
 
 function gQuestion(listOfQ) {
   listOfQ.forEach(q => {
+
     console.log(q.question.toString());
     question.innerHTML = q.question.toString();
 
-    console.log(q.correct_answer.toString());
+    /*console.log(q.correct_answer.toString());
     answers.innerHTML = q.correct_answer.toString();
     
     console.log(q.incorrect_answers.toString());
-    answers.innerHTML = q.incorrect_answers.toString();
+    answers.innerHTML = q.incorrect_answers.toString();*/
 
     console.log(answers);
   });
