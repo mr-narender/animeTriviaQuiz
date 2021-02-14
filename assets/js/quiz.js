282cc0ce39b4503711df547e0b6736943f9650c0
gammaled <guleduni@yahoo.co.uk>
2021-02-14 16:41:18 +0000

    changed value of variable 'questionCounter' to 1 from 0

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -4,7 +4,7 @@ const progressText = document.querySelector('#progress-text');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progress-bar-full');
 
-let questionCounter = 0;
+let questionCounter = 1;
 let score = 0;
 let currentQuestion = {};
 
