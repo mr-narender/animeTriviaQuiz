5d756dfa50248d758b62a1d92f391a28ab9a2880
gammaled <guleduni@yahoo.co.uk>
2021-02-15 15:51:58 +0000

    added comments in gQuestion() function to label statements and fixed indentation

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -8,44 +8,51 @@ let questionCounter = 1;
 let score = 0;
 let currentQuestion = {};
 
+
+// Function that displays question in HTML
 function gQuestion(listOfQ) {
-  listOfQ.forEach(q => {
+    listOfQ.forEach(q => {
 
-    console.log(q.question.toString());
-    question.innerHTML = q.question.toString();
+        console.log(q.question.toString());
+        question.innerHTML = q.question.toString();
 
-    q.incorrect_answers.push(q.correct_answer); 
-    
-    function shuffle(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
+        q.incorrect_answers.push(q.correct_answer); 
+        
+        //Defining shuffle() function 
+        function shuffle(array) {
+            var currentIndex = array.length, temporaryValue, randomIndex;
 
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
 
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
 
-            // And swap it with the current element.
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
+                // And swap it with the current element.
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+            }
+
+            // Return newly shuffled array
+            return array;
         }
 
-        return array;
-    }
+        //Log to console answers array
+        console.log(q.incorrect_answers.toString());
 
-    console.log(q.incorrect_answers.toString());
+        // Shuffling array containing answers
+        shuffle(q.incorrect_answers);
+        console.log(q.incorrect_answers);
 
-    // Used like so
-    shuffle(q.incorrect_answers);
-    console.log(q.incorrect_answers);
 
         for (let i = 0; i < answers.length; i++) {
         answers[i].innerHTML = q.incorrect_answers.shift().toString();
-        }
+        };
     });
-}
+};
+
 
 function questionProgress() {
 
@@ -54,11 +61,13 @@ function questionProgress() {
 
         progressText.innerText = `Question ${questionCounter} of 10`;
         progressBarFull.style.width = `${(questionCounter/10 * 100)}%`;
+        
     } else {
         return window.location.assign('/end-page.html');
     }
 
 };
+
 
 function apiQuestion() {
   fetch(`https://opentdb.com/api.php?amount=39&category=31&type=multiple`)
