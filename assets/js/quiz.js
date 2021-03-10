6fa2484f810b1ba5f2ea3806055eb2903622521c
gammaled <guleduni@yahoo.co.uk>
2021-03-10 19:26:28 +0000

    declared variable questionArray with data API as value

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,6 +3,7 @@ const answers = document.querySelectorAll('.answer-text');
 const progressText = document.querySelector('#progress-text');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progress-bar-full');
+let questionArray;
 
 let questionCounter = 1;
 let score = 0;  
@@ -143,16 +144,16 @@ function questionProgress() {
     };
 };
 
-
 function apiQuestion() {
   fetch(`https://opentdb.com/api.php?amount=39&category=31&type=multiple`)
   .then(res => res.json())
   .then(rawData => {
-      let quizData = shuffle(rawData.results); 
+      let quizData = shuffle(rawData.results);
+      questionArray = quizData; 
     gQuestion(quizData),
     answerFeedback(quizData)
+    console.log(quizData);
     });
 };
 
 apiQuestion(); 
-
