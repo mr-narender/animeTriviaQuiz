309a0d1d57cfa40ffb7ae7cc8d5493ce7f0ab922
gammaled <guleduni@yahoo.co.uk>
2021-03-17 15:42:47 +0000

    changed the method from '.querySelectorAll' to '.getElementByClassName' of the 'answers' constant

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,9 +1,9 @@
 const quizQuestion = document.querySelector('#question');
-const answers = document.querySelectorAll('.answer-text');
+const answers = document.getElementsByClassName('answer-text');
 const progressText = document.querySelector('#progress-text');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progress-bar-full');
-let correct_answer;
+let questionArray;
 
 let questionCounter = 1;
 let score = 0;  
@@ -79,7 +79,6 @@ function questionProgress() {
     };
 };
 
-let quizCorrectAnswer;
 function apiQuestion() {
   fetch(`https://opentdb.com/api.php?amount=39&category=31&type=multiple`)
   .then(res => res.json())
@@ -95,6 +94,4 @@ function apiQuestion() {
     });
 };
 
-
-console.log(quizCorrectAnswer.innerHTML);
 apiQuestion(); 
