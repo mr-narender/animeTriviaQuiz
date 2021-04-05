5d799b0d8bc11614849c24c2becd4aaf80d42c6f
gammaled <guleduni@yahoo.co.uk>
2021-04-05 14:27:51 +0000

    deleted shuffle() function that was commented out

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -23,24 +23,6 @@ function apiQuestion() {
 
 apiQuestion();
 
-/*
-function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    // Return newly shuffled array
-    return array;
-}
-*/
-
 function getQuestion(questionData) {
 
     if (questionCounter === 10) {
