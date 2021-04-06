028167c7b27e4564d64af75d9a899ed1e39202fd
gammaled <guleduni@yahoo.co.uk>
2021-04-06 06:16:11 +0000

    set a fetch call to opentbd api

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -7,4 +7,9 @@ const progressBarFull = document.querySelector('#progress-bar-full');
 
 let questionCounter = 0;  
 let score = 0;
-let questionIndex = Math.floor(Math.random() * questionData.length);
+//let questionIndex = Math.floor(Math.random() * questionData.length);
+
+fetch('https://opentdb.com/api.php?amount=39&category=31&type=multiple')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
