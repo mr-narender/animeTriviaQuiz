812cd81890301c0e35f308082491cf61426ea3d1
gammaled <guleduni@yahoo.co.uk>
2021-04-06 09:18:19 +0000

    put fetch inside function apiQuestion()

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -8,6 +8,7 @@ const progressBarFull = document.querySelector('#progress-bar-full');
 let questionCounter = 0;  
 let score = 0;
 
+function apiQuestion() {
 fetch('https://opentdb.com/api.php?amount=1&category=31&type=multiple')
     .then(res => res.json())
     .then(data => { 
@@ -22,3 +23,4 @@ fetch('https://opentdb.com/api.php?amount=1&category=31&type=multiple')
         console.log(incorrectAnswers);
     })
     .catch(err => console.error(err));
+}
