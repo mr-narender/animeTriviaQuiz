d516d5eed41edfa046536052a189ca95ed1a000a
gammaled <guleduni@yahoo.co.uk>
2021-04-22 21:53:08 +0000

    fixed comments by organising them better

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -13,17 +13,15 @@ let score = 0;
  * Used to randomise the order of the array elements
  * https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
  */
+
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
   while (0 !== currentIndex) {
 
-    // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
 
-    // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
